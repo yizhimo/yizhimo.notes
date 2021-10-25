decdd2a315fee594d6494fe10d571226e772740d
lucifer <393234692@qq.com>
2021-10-25 11:03:20 +0800

    .

---
+++ other/vue组件初始化时执行顺序.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>从源码里 initState函数里可得知,初始化时执行顺序:</h2>
+  <div>
+    beforeCreate -> inject -> Props -> Methods -> Data -> Computed -> Watch -> provide-> created
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
