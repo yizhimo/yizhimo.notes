1adf28187906f2cc651e097152347f6680fbab86
lucifer <393234692@qq.com>
2021-10-25 11:18:13 +0800

    faciliate

--- facilitate/通用数据类型判断方法.js
+++ facilitate/通用数据类型判断方法.js
@@ -11,14 +11,14 @@ function getType1(obj){
   return getType2(obj).replace(/^\[object (\S+)\]$/, '$1'); 
 }
 
-getType([])     // "Array" typeof []是object，因此toString返回
-getType('123')  // "string" typeof 直接返回
-getType(window) // "Window" toString返回
-getType(null)   // "Null"首字母大写，typeof null是object，需toString来判断
-getType(undefined)   // "undefined" typeof 直接返回
-getType()            // "undefined" typeof 直接返回
-getType(function(){}) // "function" typeof能判断，因此首字母小写
-getType(/123/g)      //"RegExp" toString返回
+getType([])            // "Array" typeof []是object，因此toString返回
+getType('123')         // "string" typeof 直接返回
+getType(window)        // "Window" toString返回
+getType(null)          // "Null"首字母大写，typeof null是object，需toString来判断
+getType(undefined)     // "undefined" typeof 直接返回
+getType()              // "undefined" typeof 直接返回
+getType(function(){})  // "function" typeof能判断，因此首字母小写
+getType(/123/g)        // "RegExp" toString返回
 
 
 /**
@@ -29,15 +29,15 @@ function getType2(obj){
   return Object.prototype.toString.call(obj).replace(/^\[object (\S+)\]$/, '$1'); 
 }
 
-getType2({})  // "[object Object]"
-getType2(1)    // "[object Number]"
-getType2('1')  // "[object String]"
-getType2(true)  // "[object Boolean]"
+getType2({})            // "[object Object]"
+getType2(1)             // "[object Number]"
+getType2('1')           // "[object String]"
+getType2(true)          // "[object Boolean]"
 getType2(function(){})  // "[object Function]"
-getType2(null)   //"[object Null]"
-getType2(undefined) //"[object Undefined]"
-getType2(/123/g)    //"[object RegExp]"
-getType2(new Date()) //"[object Date]"
-getType2([])       //"[object Array]"
-getType2(document)  //"[object HTMLDocument]"
-getType2(window)   //"[object Window]"
+getType2(null)          // "[object Null]"
+getType2(undefined)     // "[object Undefined]"
+getType2(/123/g)        // "[object RegExp]"
+getType2(new Date())    // "[object Date]"
+getType2([])            // "[object Array]"
+getType2(document)      // "[object HTMLDocument]"
+getType2(window)        // "[object Window]"
